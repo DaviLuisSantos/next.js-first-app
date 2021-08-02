bcff8778d686ebbf92debca32dd151b9f7767212
DaviLuisSantos <costasantos.daviluis@gmail.com>
2021-08-02 17:22:10 -0300

    finalzaun

--- src/pages/estatico.jsx
+++ src/pages/estatico.jsx
@@ -1,7 +1,7 @@
 import Layout from "../components/Layout";
 
 
-export function getStaticProps() {
+export default function getStaticProps() {
     return {
         props: {
             numero: Math.random()
